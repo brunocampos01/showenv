8d51a92e731f32b25d9412e946b78853a902f26d
brunocampos01 <brunocampos01@gmail.com>
2020-06-27 17:30:39 -0300

    refactor: setup

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 import os
+from setuptools import \
+    find_packages  # get modules (pyssas) and submodules (cube_formatter, ...)
 from setuptools import setup
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
+requirements_path = os.path.join(HERE, 'requirements.txt')
+install_reqs = parse_requirements(requirements_path, session=False)
+
+try:
+    requirements = [str(ir.req) for ir in install_reqs]
+except AttributeError:
+    requirements = [str(ir.requirement) for ir in install_reqs]
 
 # The text of the README file
 with open(os.path.join(HERE, "README.md")) as f:
@@ -11,26 +25,22 @@ with open(os.path.join(HERE, "README.md")) as f:
 
 setup(name='easyenv',
       version='0.1',
-      description='',
+      packages=find_packages(),
+      install_requires=requirements,
+            entry_points={
+          "console_scripts": ["easyenv=easyenv.__main__:main"]
+      },
+      description='Prepare environment to run Data Science applications',
       long_description=README,
       long_description_content_type="text/markdown",
       url='https://github.com/brunocampos01/easyenv',
       author='Bruno Campos',
       author_email="brunocampos01@gmail.com",
       license='MIT',
-      platforms='any',
+      platforms='linux',
       classifiers=[
-          'Environment :: Terminal',
           'Programming Language :: Python',
-          'Development Status :: 4 - Beta',
           'Natural Language :: English',
-          'Intended Audience :: Developers, Data Engineers, Data Scientists',
-          'License :: MIT',
-          'Operating System :: OS Independent',
-          'Topic :: Software Development :: Libraries :: Python Modules'
+          'License :: OSI Approved :: MIT License'
           ],
-      packages=["easyenv"],
-      entry_points={
-          "console_scripts": ["easyenv=easyenv.__main__:main"]
-      },
       zip_safe=False)
